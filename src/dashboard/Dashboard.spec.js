9c2f883879e1533679cbf67b60face3f9ebd5d11
coriagogo <pkcparis@gmail.com>
2019-06-19 16:02:04 -0400

    dashboard tests completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,25 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import renderer from  'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('matches the snapshot', () => {
+    const { container } = render(<Dashboard />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders without issue', () => {
+    render(<Dashboard />);
+  });
+
+  it('renders all children', () => {
+    render(<Dashboard />);
+
+    expect(document.querySelector('.controls')).toBeTruthy();
+    expect(document.querySelector('.display')).toBeTruthy();
+  });
+});
